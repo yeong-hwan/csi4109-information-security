2fdd06a3b0e175b5ced2f3db5c1fa0702062816a
yeong-hwan <jghff700@naver.com>
2024-03-12 14:30:29 +0900

    feat: append state struct

--- assignment-1/src/main.c
+++ assignment-1/src/main.c
@@ -11,22 +11,30 @@ typedef struct House
 
     char *owner_name;
     char **keys;
+
+    // insides
     struct Node *head;
 
 } House;
 
+typedef struct State
+{
+    char *user_name;
+    char *key;
+    bool allowed;
+} State;
+
 int main(int argc, char *argv[])
 {
-    // printf("%d\n", argc);
-    // printf("%s\n", argv[1]);
-
     if (argc < 3)
     {
         printf("%s\n", "CHECK YOUR USAGE: ./secure_house <owner_name> <key_1> <key_2> ... <key_n>");
         return 0;
     }
 
+    // House initial constructor
     House house;
+    State state;
     house.owner_name = argv[1];
 
     int key_count = argc - 2;
@@ -40,38 +48,95 @@ int main(int argc, char *argv[])
 
     house.head = NULL;
 
-    // printf("%s\n", house.owner_name);
-
-    // for (int i = 0; i < key_count; i++)
-    // {
-    //     printf("%s\n", house.keys[i]);
-    // }
-
     char *line = NULL;          // Pointer to hold the line
     size_t bufsize = 0;         // Size of the buffer
     ssize_t linelen;            // Length of the line read
     char *token;                // Pointer to hold each token
     char delimiter[] = " \t\n"; // Delimiters (space, tab, newline)
 
+    char *user_name;
+    char *key_now;
+
+    int idx = 0;
+
     // Read lines until EOF
     while ((linelen = getline(&line, &bufsize, stdin)) != -1)
     {
+        idx++;
+
         // If the line ends with '\n', remove it
         if (line[linelen - 1] == '\n')
             line[linelen - 1] = '\0';
 
         // Print the line
-        printf("\n%s\n", line);
+        printf("\n%d. ", idx);
+        printf("%s\n", line);
 
         // Tokenize the line by space, tab, or newline
         token = strtok(line, delimiter);
-        while (token != NULL)
+        // printf("%s\n", token);
+
+        // End program at EOF
+        if (token == NULL)
         {
-            // Print each token
-            printf("%s\n", token);
-            // Get the next token
+            break;
+        }
+
+        if (strcmp(token, "INSERT") == 0)
+        { // pass "KEY" token
+            token = strtok(NULL, delimiter);
+            user_name = strtok(NULL, delimiter);
+            key_now = strtok(NULL, delimiter);
+
+            printf("KEY %s INSERTED BY %s\n", key_now, user_name);
+        }
+        else if (strcmp(token, "TURN") == 0)
+        { // pass "KEY" token
+            token = strtok(NULL, delimiter);
+            user_name = strtok(NULL, delimiter);
+
+            for (int i = 0; i < key_count; i++)
+            {
+                // printf("%s\n", house.keys[i]);
+                if (strcmp(key_now, house.keys[i]) == 0)
+                {
+                    printf("SUCCESS %s TURNS KEY %s\n", user_name, key_now);
+                    break;
+                }
+            }
+        }
+        else if (strcmp(token, "ENTER") == 0)
+        { // pass "HOUSE" token
+            token = strtok(NULL, delimiter);
+
+            user_name = strtok(NULL, delimiter);
+        }
+        else if (strcmp(token, "WHO'S") == 0)
+        { // pass "INSIDE?" token
             token = strtok(NULL, delimiter);
         }
+        else if (strcmp(token, "CHANGE") == 0)
+        { // pass "LOCKS" token
+            token = strtok(NULL, delimiter);
+
+            user_name = strtok(NULL, delimiter);
+
+            while (token != NULL)
+            {
+                // Print each token
+                printf("%s\n", token);
+                // Get the next token
+                token = strtok(NULL, delimiter);
+            }
+        }
+        else if (strcmp(token, "LEAVE") == 0)
+        { // pass "HOUSE" token
+            token = strtok(NULL, delimiter);
+
+            user_name = strtok(NULL, delimiter);
+        }
+
+        // printf("user_name: %s\n", user_name);
     }
 
     // Free the memory allocated for the line
